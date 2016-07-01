1b2c1683d4dd2559c00969f7ded3e160997fb26d
Emanuel <Emanuel@DECKELE-PC>
2016-07-01 13:14:16 +0300

    New project - FileFinder

---
+++ HW5 for the 7.7.16/FileFinder/FileFinder/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if ((args[0] == null) || (args[1] == null))
+            {
+                Console.WriteLine("Error: Invalid arguments.");
+                return;
+            }
+            if (!Directory.Exists(args[0]))
+            {
+                Console.WriteLine("Error: Directory doesn't exist.");
+                return;
+            }
+        }
+    }
+}
