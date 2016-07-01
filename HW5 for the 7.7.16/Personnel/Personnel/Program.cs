580dba8ea5f5cdde087927d9dbe1aa085eb09fa3
Emanuel <Emanuel@DECKELE-PC>
2016-07-01 12:34:58 +0300

    Added new project Personnel

---
+++ HW5 for the 7.7.16/Personnel/Personnel/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Xsl;
+
+namespace Personnel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> nameList = ReadNames("NamesForExercise.txt");
+            foreach (var name in nameList)
+            {
+                Console.WriteLine(name);
+            }
+        }
+
+        public static List<string> ReadNames(string path)
+        {
+            var nameList = new List<string>();
+            var reader = new StreamReader(path);
+            while (!reader.EndOfStream)
+            {
+                nameList.Add(reader.ReadLine());
+            }
+            reader.Close();
+            return nameList;
+        }
+    }
+}
