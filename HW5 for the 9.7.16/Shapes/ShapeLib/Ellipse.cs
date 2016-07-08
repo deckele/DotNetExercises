8db26255bd1f07195c20b2c3e68c3d3275fdfb46
Emanuel <Emanuel@DECKELE-PC>
2016-07-08 10:48:12 +0300

    Fixed previouse code

---
+++ HW5 for the 9.7.16/Shapes/ShapeLib/Ellipse.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text;
+
+namespace ShapeLib
+{
+    public class Ellipse : Shape, IPersist, IComparable
+    {
+        public float Hight { get; private set; }
+        public float Width { get; private set; }
+
+        public Ellipse(float hight, float width)
+        {
+            Hight = hight;
+            Width = width;
+        }
+        public Ellipse(ConsoleColor color, float hight, float width) : base(color)
+        {
+            Hight = hight;
+            Width = width;
+        }
+
+        public override float Area
+        {
+            get
+            {
+                return (float)Math.PI * Hight * Width / 4;
+            }
+        }
+        public override void Display()
+        {
+            base.Display();
+            Console.WriteLine("The major axis of your ellipse is: {0}.\nThe minor axis of your ellipse is: {1}.", 
+                Math.Max(Hight, Width), Math.Min(Hight, Width));
+        }
+
+        //Overriding Write() Method for the implementing of IPersist.
+        public virtual void Write(StringBuilder sb)
+        {
+            sb.Append("Major axis of ellips is: ").Append(Math.Max(Hight, Width).ToString()).
+                Append(", minor axis of ellipse is: ").Append(Math.Min(Hight, Width).ToString()).AppendLine(".");
+        }
+        //Overriding CompareTo() Method for the implementing of IComparable.
+        public int CompareTo(Object obj)
+        {
+            if (obj == null)
+            {
+                return 1;
+            }
+            Shape shape = obj as Shape;
+            if (shape != null)
+            {
+                return this.Area.CompareTo(shape.Area);
+            }
+            else
+            {
+                throw new ArgumentException("Object compared is not a \"shape\".");
+            }
+        }
+    }
+}
---
+++ HW5 for the 9.7.16/Shapes/ShapeLib/Shape.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ShapeLib
+{
+    public abstract class Shape
+    {
+        public abstract float Area{get;}
+        protected ConsoleColor Color {get; private set;}
+
+        internal Shape(ConsoleColor color)
+        {
+            Color = color;
+        }
+        internal Shape() : this(ConsoleColor.White) { }
+        //Sets console background color according to the Color property.
+        public virtual void Display()
+        {
+            Console.BackgroundColor = Color;
+        }
+    }
+}
