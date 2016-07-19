011b4af57c7bcbe06a456c62b6796b4df101590f
Emanuel <Emanuel@DECKELE-PC>
2016-07-20 02:22:22 +0300

    4.1 (a) done

---
+++ HW7 for the 23.7.16/LINQ4.1/LINQ4.1/AssemblyPublicInterfaceQuarry.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ4._1
+{
+    internal class AssemblyPublicInterfaceQuarry
+    {
+        public void DisplaySortedList(Assembly assemblyToQuarry)
+        {
+            var typsInAssembly = assemblyToQuarry.GetExportedTypes().Where(type => type.IsInterface);
+            var typsInAssemblyList = typsInAssembly.OrderBy(type => type.TypeHandle.ToString()).ToList();
+
+            Console.WriteLine($"List of public Interfaces and number of methods in each type for assembly- {assemblyToQuarry}:");
+            foreach (var type in typsInAssemblyList)
+            {
+                Console.WriteLine(type);
+                Console.WriteLine($"Number of methods in public interface: {type.GetMethods().Count()}.");
+            }
+        }
+    }
+}
+
---
+++ HW7 for the 23.7.16/LINQ4.1/LINQ4.1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var assemblyToQuarry = new AssemblyPublicInterfaceQuarry();
+            assemblyToQuarry.DisplaySortedList(typeof(string).Assembly);
+        }
+    }
+}
