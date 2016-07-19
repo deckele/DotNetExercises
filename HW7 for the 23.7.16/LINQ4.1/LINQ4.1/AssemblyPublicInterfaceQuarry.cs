f017bcc59bb4ca290143c569baa49ccf83b5564f
Emanuel <Emanuel@DECKELE-PC>
2016-07-20 02:43:57 +0300

    Made LINQ method awsome with LINQ syntax sugar...

--- HW7 for the 23.7.16/LINQ4.1/LINQ4.1/AssemblyPublicInterfaceQuarry.cs
+++ HW7 for the 23.7.16/LINQ4.1/LINQ4.1/AssemblyPublicInterfaceQuarry.cs
@@ -11,11 +11,17 @@ namespace LINQ4._1
     {
         public void DisplaySortedList(Assembly assemblyToQuarry)
         {
-            var typsInAssembly = assemblyToQuarry.GetExportedTypes().Where(type => type.IsInterface);
-            var typsInAssemblyList = typsInAssembly.OrderBy(type => type.TypeHandle.ToString()).ToList();
+            //Alternative code without LINQ syntax sugar:
+            //var typsInAssembly = assemblyToQuarry.GetExportedTypes().Where(type => type.IsInterface);
+            //var typsInAssemblyList = typsInAssembly.OrderBy(type => type.TypeHandle.ToString()).ToList();
+
+            var typsInAssembly = from type in assemblyToQuarry.GetExportedTypes()
+                where type.IsInterface
+                orderby type.TypeHandle.ToString()
+                select type;
 
             Console.WriteLine($"List of public Interfaces and number of methods in each type for assembly- {assemblyToQuarry}:");
-            foreach (var type in typsInAssemblyList)
+            foreach (var type in typsInAssembly)
             {
                 Console.WriteLine(type);
                 Console.WriteLine($"Number of methods in public interface: {type.GetMethods().Count()}.");
