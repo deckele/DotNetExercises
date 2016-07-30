bd9cb3e587f04614d0f9ae61ddfe59b6db3531c9
Emanuel <Emanuel@DECKELE-PC>
2016-07-31 01:59:26 +0300

    Finished LimitedQueue solution.

--- HW8 for the 30.7.16/Queues/Queues/LimitedQueue .cs
+++ HW8 for the 30.7.16/Queues/Queues/LimitedQueue .cs
@@ -2,11 +2,66 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Queues
 {
-    class LimitedQueue<T>
+    internal class LimitedQueue<T>
     {
+        private readonly Queue<T> _queue;
+        private readonly SemaphoreSlim _semaphore;
+        private readonly ReaderWriterLockSlim _lock = new ReaderWriterLockSlim(LockRecursionPolicy.NoRecursion);
+
+        public LimitedQueue(int maxQueued)
+        {
+            _queue = new Queue<T>();
+            _semaphore = new SemaphoreSlim(maxQueued);
+        }
+
+        public void Enque(T item)
+        {
+            _semaphore.Wait();
+
+            _lock.TryEnterWriteLock(-1);
+
+            try
+            {
+                _queue.Enqueue(item);
+            }
+            finally
+            {
+                _lock.ExitWriteLock();
+            }
+        }
+
+        public T Deque()
+        {
+            T item;
+
+            _lock.TryEnterWriteLock(-1);
+
+            _semaphore.Release();
+
+            try
+            {
+                item = _queue.Dequeue();
+            }
+            finally
+            {
+                _lock.ExitWriteLock();
+            }
+
+            _lock.TryEnterReadLock(-1);
+
+            try
+            {
+                return item;
+            }
+            finally
+            {
+               _lock.ExitReadLock(); 
+            }
+        }
     }
 }
--- HW8 for the 30.7.16/Queues/Queues/Program.cs
+++ HW8 for the 30.7.16/Queues/Queues/Program.cs
@@ -10,6 +10,36 @@ namespace Queues
     {
         static void Main(string[] args)
         {
+            var random = new Random();
+            const int maxQueued = 3;
+            const int taskNumber = 10;
+
+            var limitedQueue = new LimitedQueue<int>(maxQueued);
+            var tasks = new Task[taskNumber];
+
+            for(int i = 0; i < 10; i++)
+            {
+                int tastkId = i;
+
+                tasks[i] = Task.Run(async () =>
+                {
+                    
+                    var randomNumber = random.Next(1, 1000);
+
+                    await Task.Delay(random.Next(1000, 3000));
+                    Console.WriteLine($"Task {tastkId} begins and waits for Enque.");
+                    limitedQueue.Enque(randomNumber);
+                    Console.WriteLine($"Task {tastkId} added {randomNumber} To Queue.");
+                    await Task.Delay(random.Next(1000, 2000));
+                    Console.WriteLine($"Task {tastkId} continues and tries to Deque.");
+                    Console.WriteLine($"Task {tastkId} removed {limitedQueue.Deque()} from queue.");
+                });
+            }
+
+            Task.WaitAll(tasks);
+
+            Console.WriteLine();
+            Console.WriteLine("All tasks finished. No DeadLock detected.");
         }
     }
 }
