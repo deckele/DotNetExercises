bc7eab164941ecf9f8ac74201520327eebc0d067
Emanuel <Emanuel@DECKELE-PC>
2016-07-31 02:38:46 +0300

    Cosmetic

---
+++ HW8 for the 30.7.16/SyncDemo/SyncDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+namespace SyncDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mutex = new Mutex(false, "SyncFileMutex");
+
+            for (var i = 0; i < 10000; i++)
+            {
+                mutex.WaitOne();
+                File.AppendAllText(@"C:\Temp\data.txt", Process.GetCurrentProcess().Id.ToString() + Environment.NewLine);
+                mutex.ReleaseMutex();
+            }
+        }
+    }
+}
