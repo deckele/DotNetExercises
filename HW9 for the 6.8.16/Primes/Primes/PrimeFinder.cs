9c36df86c364a9fbc1495ca21510bfc9e94a3670
Emanuel <Emanuel@DECKELE-PC>
2016-08-09 22:03:06 +0300

    Finished Primes solution

---
+++ HW9 for the 6.8.16/Primes/Primes/PrimeFinder.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Primes
+{
+    internal class PrimeFinder
+    {
+        /// <summary>
+        /// Returns an integer array of all prime numbers between input 1 and input 2 of user.
+        /// </summary>
+        /// <param name="input1"></param>
+        /// <param name="input2"></param>
+        /// <param name="degreeOfParallelism"></param>
+        /// <returns></returns>
+        public static int[] CalcPrimes(int input1, int input2, int degreeOfParallelism)
+        {
+            var lockerObject = new object();
+            var primesRange = Enumerable.Range(input1, input2);
+            var maxParallelism = new ParallelOptions()
+            {
+                MaxDegreeOfParallelism = degreeOfParallelism
+            };
+
+            var primeList = new ArrayList();
+
+            Parallel.ForEach(primesRange, maxParallelism, (number) =>
+            {
+                if (PrimeFinder.CheckPrime(number))
+                {
+                    lock (lockerObject)
+                    {
+                        primeList.Add(number);
+                    }
+                }
+            });
+
+            var primeArray = new int[primeList.Count];
+            primeList.CopyTo(primeArray);
+            return primeArray;
+        }
+
+        /// <summary>
+        /// Checks if given argument is a prime number.
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        private static bool CheckPrime(int number)
+        {
+            if (number < 0)
+            {
+                return false;
+            }
+
+            if ((number & 1) == 0)
+            {
+                if (number == 2)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            for (var i = 3; (i*i) <= number; i += 2)
+            {
+                if ((number%i) == 0)
+                {
+                    return false;
+                }
+            }
+            return number != 1;
+        }
+    }
+}
--- HW9 for the 6.8.16/Primes/Primes/Program.cs
+++ HW9 for the 6.8.16/Primes/Primes/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,16 @@ namespace Primes
     {
         static void Main(string[] args)
         {
+            var parallelismTimer = new Stopwatch();
+
+            for (int i = 1; i <= 8; i++)
+            {
+                parallelismTimer.Start();
+                PrimeFinder.CalcPrimes(0, 1000000, i);
+                Console.WriteLine($"Time in milliseconds for CalcPrimes using {i} max tasks: {parallelismTimer.ElapsedMilliseconds}.");
+                parallelismTimer.Reset();
+                Console.WriteLine();
+            }
         }
     }
 }
