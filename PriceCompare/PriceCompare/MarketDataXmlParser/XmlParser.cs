b0c902bdb32e9f8a334877b5c00f258f6d436f70
Emanuel <Emanuel@DECKELE-PC>
2016-08-24 00:30:57 +0300

    parser work

--- PriceCompare/PriceCompare/MarketDataXmlParser/XmlParser.cs
+++ PriceCompare/PriceCompare/MarketDataXmlParser/XmlParser.cs
@@ -10,34 +10,61 @@ namespace MarketDataXmlParser
 {
     public class XmlParser
     {
-        private string _path;
+        private string _filePath;
 
         public XmlParser()
         {
-            _path = null;
+            _filePath = null;
         }
-        public XmlParser(string path)
+        public XmlParser(string filePath)
         {
-            _path = path;
+            _filePath = filePath;
         }
 
-        public void XmlQuery()
+        public IEnumerable<double> XmlIntQuery(string elementName)
         {
-            XmlQuery(_path);
+            return XmlIntQuery(_filePath, elementName);
         }
-        public void XmlQuery(string path)
+        public IEnumerable<double> XmlIntQuery(string filePath, string elementName)
         {
-            _path = path;
+            _filePath = filePath;
 
-            var doc = XDocument.Load(@"C:\Users\amirc_000\Desktop\ConsoleApplication1\ConsoleApplication1\XMLFile1.xml");
-            var prices = from items in doc.Root?.Elements()
-                         where items.Elements().Any(child => child.Element("Price") != null)
-                         from item in items.Elements()
-                         let price = item.Element("Price")?.Value
-                         where price != null
-                         select int.Parse(price);
+            var doc = XDocument.Load(@"D:\Program Files\GO\GoProjects\bin\PriceFull7290725900003_001_201608140516.xml");
+            var doubleElements = from items in doc.Root?.Elements()
+                         where items.Elements().Any(child => child.Element(elementName) != null)
+                                from item in items.Elements()
+                                let doubleElement = item.Element(elementName)?.Value
+                                where doubleElement != null
+                                select double.Parse(doubleElement);
+            return doubleElements;
+        }
 
+        public IEnumerable<string> XmlStringQuery(string elementName)
+        {
+            return XmlStringQuery(_filePath, elementName);
+        }
+        public IEnumerable<string> XmlStringQuery(string filePath, string elementName)
+        {
+            _filePath = filePath;
 
+            var doc = XDocument.Load(@"D:\Program Files\GO\GoProjects\bin\PriceFull7290725900003_001_201608140516.xml");
+            var stringElements = from items in doc.Root?.Elements()
+                              where items.Elements().Any(child => child.Element(elementName) != null)
+                                    from item in items.Elements()
+                                    let stringElement = item.Element(elementName)?.Value
+                                    where stringElement != null
+                                    select stringElement.ToLower();
+            return stringElements;
+        }
+
+        public void FileExtract(string folderPath)
+        {
+            
+        }
+
+        public void XmlParseAll()
+        {
+            UnicodeEncoding = Encoding.ASCII;
         }
     }
 }
