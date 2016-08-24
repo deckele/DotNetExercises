5935027ad68c8238759481835ffbb4f3e0a607c4
Emanuel <Emanuel@DECKELE-PC>
2016-08-25 01:26:23 +0300

    asp.net

---
+++ PriceCompare/PriceCompare/SuperComparingApp/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SuperComparingApp.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ PriceCompare/PriceCompare/SuperComparingApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SuperComparingApp.WebApiApplication" Language="C#" %>
