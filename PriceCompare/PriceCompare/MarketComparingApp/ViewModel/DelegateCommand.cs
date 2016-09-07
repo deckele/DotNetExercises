1d670806ee9eefe110cd5bba8560d492e1358956
Emanuel <Emanuel@DECKELE-PC>
2016-09-07 09:48:33 +0300

    ViewModel commands

---
+++ PriceCompare/PriceCompare/MarketComparingApp/ViewModel/DelegateCommand.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MarketComparingApp
+{
+    public interface ICommandEx : ICommand
+    {
+        string Header { get; }
+    }
+
+    public class DelegateCommand : ICommandEx
+    {
+        public string Header { get; }
+        private readonly Action _execute;
+        private readonly Func<bool> _canExecute;
+
+        public DelegateCommand(Action execute, Func<bool> canExecute = null, string header = null)
+        {
+            Header = header;
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute();
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute();
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+    public class DelegateCommand<T> : ICommandEx
+    {
+        public string Header { get; }
+        private readonly Action<T> _execute;
+        private readonly Func<T, bool> _canExecute;
+
+        public DelegateCommand(Action<T> execute, Func<T, bool> canExecute = null, string header = null)
+        {
+            Header = header;
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute((T)parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute((T)parameter);
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
